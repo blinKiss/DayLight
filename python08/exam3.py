27683ca29c061d7ba4f215a8a194f232d5b229c3
blinKiss <blinkiss@naver.com>
2023-02-16 16:17:55 +0900

    16

--- python08/exam3.py
+++ python08/exam3.py
@@ -10,12 +10,13 @@ import pandas as pd
 df = pd.DataFrame({
     'prod_id' : ['com-10000', 'car-10000', 'com-20000', 'sport-10000'],
     'prod_name' : ['삼성노트북', '엔진오일', '키보드', '등산화'],
-    'prod_price' : ['₩1,000,000', '₩50,000', '₩100,000', '₩80,000']
-    # 'prod_price' : [1000000, 50000, 100000, 80000]
+    # 'prod_price' : ['₩1,000,000', '₩50,000', '₩100,000', '₩80,000']
+    'prod_price' : [1000000, 50000, 100000, 80000]
 })
 
-df['sale_price'] = ['₩1,100,000', '₩60,000', '₩120,000', '₩90,000']
-# df['sale_price'] = [i*0.8 for i in df['prod_price']]
+# df['sale_price'] = ['₩1,100,000', '₩60,000', '₩120,000', '₩90,000']
+df['sale_price'] = ['₩' + str(format(round((i*1.2)), ',')) for i in df['prod_price']]
+df['prod_price'] = ['₩' + str(format(i, ',')) for i in df['prod_price']]
 
 
 
