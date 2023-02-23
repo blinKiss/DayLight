6a27387b3d3daaefdd57c804f3b563ae33b9f149
blinKiss <blinkiss@naver.com>
2023-02-23 13:55:03 +0900

    23

--- python09/exam5.py
+++ python09/exam5.py
@@ -1,14 +1,13 @@
 import pandas as pd
 
-df1 = pd.read_csv('./data/소상공인시장진흥공단_상가(상권)정보_대전_202212.csv')
-df2 = pd.read_csv('./data/소상공인시장진흥공단_상가(상권)정보_대구_202212.csv')
-df3 = pd.read_csv('./data/소상공인시장진흥공단_상가(상권)정보_제주_202212.csv')
-df4 = pd.read_csv('./data/소상공인시장진흥공단_상가(상권)정보_세종_202212.csv')
-df5 = pd.read_csv('./data/소상공인시장진흥공단_상가(상권)정보_울산_202212.csv')
+df1 = pd.read_csv('./data/소상공인시장진흥공단_상가(상권)정보_대전_202212.csv', low_memory=False)
+df2 = pd.read_csv('./data/소상공인시장진흥공단_상가(상권)정보_대구_202212.csv', low_memory=False)
+df3 = pd.read_csv('./data/소상공인시장진흥공단_상가(상권)정보_제주_202212.csv', low_memory=False)
+df4 = pd.read_csv('./data/소상공인시장진흥공단_상가(상권)정보_세종_202212.csv', low_memory=False)
+df5 = pd.read_csv('./data/소상공인시장진흥공단_상가(상권)정보_울산_202212.csv', low_memory=False)
 
 df_total = pd.concat([df1, df2, df3, df4, df5])
 df_total_view = df_total[['상호명', '상권업종중분류명']]
-# print(df_total_view.head(n=5))
 # df_bakery = df_total_view[df_total_view['상권업종중분류명'].str.contains('제과')]
 df_bakery = df_total_view[df_total_view['상권업종중분류명'] == '제과제빵떡케익']
 # print(df_bakery)
@@ -20,4 +19,3 @@ if((paris.shape[0] > tous.shape[0]) == True):
     print('빠바가 더 많음')
 else:
     print('뚜쥬가 더 많음')
-
---
+++ python09/exam6.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_excel('./data/codefilex.xlsx')
+
+print('전국 은행 지점 수 :', df.shape[0])
+df_slice = df[['은행코드', '점포명', '주소', '전화번호']]
+# print(df_slice)
+df_woori = df_slice[df_slice['점포명'].str.contains('우리.*은행')]
+print(df_woori)
+
+
+
